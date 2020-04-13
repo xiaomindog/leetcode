150de62029a730d9ef7d8aae48d4c2c7e08bb2ed
xiaomindog <1131651415@qq.com>
2020-04-13 19:05:29 +0800

    287

---
+++ java_leetcode/src/com/leetcode/medium/findDuplicate_287.java
@@ -0,0 +1,50 @@
+package com.leetcode.medium;
+
+/*给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+假设只有一个重复的整数，找出这个重复的数。
+示例 1:输入: [1,3,4,2,2]输出: 2
+示例 2:输入: [3,1,3,4,2]输出: 3
+说明：不能更改原数组（假设数组是只读的）。只能使用额外的O(1)的空间。时间复杂度小于 O(n2) 。
+数组中只有一个重复的数字，但它可能不止重复出现一次。
+*/
+public class findDuplicate_287 {
+    public static int findDuplicate(int[] nums) {
+        if (nums.length == 0) return 0;
+        while (true) {
+            int temp = nums[0];
+            nums[0] = nums[temp];
+            nums[temp] = temp;
+            if (nums[0] == nums[temp]) {
+                break;
+            }
+        }
+        return nums[0];
+    }
+
+    //参考题解的 二分法思路
+    public static int findDuplicate_2(int[] nums) {
+        int left = 1, right = nums.length - 1;
+
+        while (left < right) {
+            int count = 0;
+            int mid = (right - left) / 2 + left;
+            for (int i : nums) {
+                if (i <= mid) {
+                    count++; //统计区间内小于等于mid的个数
+                }
+            }
+            if (count > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {};
+        int s = findDuplicate(nums);
+        System.out.println(s);
+    }
+}
